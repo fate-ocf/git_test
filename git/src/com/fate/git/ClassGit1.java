4da2e85ec4a48a28f6284a6fe3f7a6988f210e64
fate <fate@qq.com>
2019-12-30 13:32:12 +0800

    添加一个常量i

--- git/src/com/fate/git/ClassGit1.java
+++ git/src/com/fate/git/ClassGit1.java
@@ -2,6 +2,8 @@ package com.fate.git;
 
 public class ClassGit1 {
     public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
         System.out.println("hello git");
     }
 }
