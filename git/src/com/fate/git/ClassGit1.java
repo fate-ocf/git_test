8fd6ab24f012d34d434ae14989ac19c788453469
fate <fate@qq.com>
2019-12-30 13:38:23 +0800

    添加第二行注释

--- git/src/com/fate/git/ClassGit1.java
+++ git/src/com/fate/git/ClassGit1.java
@@ -3,6 +3,7 @@ package com.fate.git;
 public class ClassGit1 {
     public static void main(String[] args) {
         //这是一行注释
+        //再次修改
         int i = 10;
         System.out.println(i);
         System.out.println("hello git");
